8013f6c2f5715e3f06dc188955adfd9921a1334a
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-08-13 23:52:44 +0100

    add: layout

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,6 @@ export default function Page() {
       {/* <div className={styles.shape} /> */}
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
-        />
           <p className={`${lusitana.className}  text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
@@ -46,7 +44,7 @@ export default function Page() {
             src="/hero-mobile.png"
             width={560}
             height={500}
-            className="md:hidden"
+            className="block md:hidden"
             alt="dshboard mobile version"
           />
         </div>
