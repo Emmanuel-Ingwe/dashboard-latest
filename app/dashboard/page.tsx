955339c0e51c6b59b48f5010b73c9a738f3d4430
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-08-23 17:25:18 +0100

    add: suspense/>

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,8 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
@@ -30,6 +32,9 @@ export default async function Page() {
         /> 
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
