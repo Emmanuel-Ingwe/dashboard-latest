98cd54f62e18afc6d6815338f50c89006e54b315
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-08-24 23:48:33 +0100

    add: ppr

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+
+export const experimental_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
