835dff79298b67d941e3884f850adac59961d76c
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-09-15 10:29:57 +0100

    fix

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,16 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
          const searchParams = useSearchParams();
          const pathname = usePathname();
          const { replace } = useRouter();
 
-      function handleSearch(term: string) {
+         const handleSearch = useDebouncedCallback((term) => {
         // console.log(`Searching... ${term}`);
+
         const params = new URLSearchParams(searchParams);
         if (term) {
           params.set('query', term);
@@ -18,7 +20,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
         }
         replace(`${pathname}?${params.toString()}`);
         console.log(term);
-      }
+      })
+      
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
