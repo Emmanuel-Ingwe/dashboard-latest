80f462bd2b0dda5968412bd2d3db66361317fbf1
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-09-22 15:09:05 +0100

    chnge

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,11 +13,10 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    };
-    // Test it out:
-    console.log(rawFormData);
+     const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
   }
