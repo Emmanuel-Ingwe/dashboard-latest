58d1d7885a518495e1ab03c79b1802ed1bf002eb
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-09-22 15:11:24 +0100

    ,,

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,11 +15,16 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-     const { customerId, amount, status } = CreateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
+        const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
   });
-  const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
+        const amountInCents = amount * 100;
+        const date = new Date().toISOString().split('T')[0];
+
+         await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
   }
