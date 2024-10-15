e5241e24d9c275a5d2e31d19d3c07a76c35d1c30
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2024-10-15 17:04:17 +0100

    2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,10 @@ import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
-  title: 'Dre_vil Dashboard!',
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
   description: 'The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
